6344b7ebd5df557fa8467dc22330f6b48d8f5765
zhouwl <zhouwl@missfress.cn>
2019-02-07 20:08:02 +0800

    代码优化

--- schedule.go
+++ schedule.go
@@ -8,7 +8,7 @@ var schedule []map[string]func()
 //
 //时间单位 秒
 func Schedule(name string, timeSchedule int, fun func()) {
-	mLogger.InfoLn("注册定时任务: %v 间隔时间: %v", name, timeSchedule)
+	mLogger.InfoF("注册定时任务: %v 间隔时间: %v", name, timeSchedule)
 	go func(timeSchedule int, fun func()) {
 		for {
 			time.Sleep(time.Second * time.Duration(timeSchedule))
