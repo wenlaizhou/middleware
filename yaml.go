b422c02e1d77a6cdc987c1bf67cfe7661f1f16ad
zhouwl <zhouwl@missfress.cn>
2019-02-18 10:24:10 +0800

    代码优化

--- yaml.go
+++ yaml.go
@@ -1,7 +1,8 @@
 package middleware
 
-import "gopkg.in/yaml.v2"
-
-func ParseYaml() {
-	yaml.Marshal(nil)
-}
+//
+// import "gopkg.in/yaml.v2"
+//
+// func ParseYaml() {
+// 	yaml.Marshal(nil)
+// }
