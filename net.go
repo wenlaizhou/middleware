c460a80ca8ee24ac5e791ad4c1628eea1b8ce1be
zhouwl <zhouwl@missfress.cn>
2019-03-27 16:37:09 +0800

    新增json数据处理接口

--- net.go
+++ net.go
@@ -5,14 +5,25 @@ import (
 	"net"
 )
 
-// 测试网络连通
-// TCP, UDP
-func IsActive(ip string, port int) bool {
+const (
+	PROTOCOL_TCP = "tcp"
+	PROTOCOL_UDP = "udp"
+)
 
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%v", ip, port))
-	if err != nil {
+// 测试网络连通
+// tcp, udp
+func IsActive(protocol string, ip string, port int) bool {
+	switch protocol {
+	case PROTOCOL_TCP:
+	case PROTOCOL_UDP:
+		conn, err := net.Dial(protocol, fmt.Sprintf("%s:%v", ip, port))
+		if err != nil {
+			return false
+		}
+		defer conn.Close()
+		return true
+	default:
 		return false
 	}
-	conn.Close()
-	return true
+	return false
 }
