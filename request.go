71bb50f6970d87f9758befd3ae8a2d56b7147974
zhouwl <zhouwl@missfress.cn>
2019-08-07 17:47:45 +0800

    bug fix

--- request.go
+++ request.go
@@ -7,13 +7,46 @@ import (
 	"io/ioutil"
 	"net/http"
 	"reflect"
+	"time"
 )
+
+var defaultTimeout = 30
+
+// return : statusCode, header, body, error
+func PostWithTimeout(timeoutSecond int, url string, contentType string, data []byte) (int, map[string][]string, []byte, error) {
+	return PostFull(timeoutSecond, url, nil, contentType, data)
+}
+
+// return : statusCode, header, body, error
+func PostJsonWithTimeout(timeoutSecond int, url string, data interface{}) (int, map[string][]string, []byte, error) {
+	return PostJsonFull(timeoutSecond, url, nil, data)
+}
+
+// return : statusCode, header, body, error
+func GetWithTimeout(timeoutSecond int, url string) (int, map[string][]string, []byte, error) {
+	return GetFull(timeoutSecond, url, nil)
+}
+
+// return : statusCode, header, body, error
+func Post(url string, contentType string, data []byte) (int, map[string][]string, []byte, error) {
+	return PostFull(defaultTimeout, url, nil, contentType, data)
+}
+
+// return : statusCode, header, body, error
+func PostJson(url string, data interface{}) (int, map[string][]string, []byte, error) {
+	return PostJsonFull(defaultTimeout, url, nil, data)
+}
+
+// return : statusCode, header, body, error
+func Get(url string) (int, map[string][]string, []byte, error) {
+	return GetFull(defaultTimeout, url, nil)
+}
 
 // Post: post data to url
 //
 // return : statusCode, header, body, error
-func Post(url string, headers map[string]string, data []byte) (int, map[string][]string, []byte, error) {
-	return DoRequest(POST, url, headers, "", data)
+func PostFull(timeoutSecond int, url string, headers map[string]string, contentType string, data []byte) (int, map[string][]string, []byte, error) {
+	return DoRequest(timeoutSecond, POST, url, headers, contentType, data)
 }
 
 // PostJson: post json data to url, contentType设置为: application/json utf8
@@ -21,29 +54,28 @@ func Post(url string, headers map[string]string, data []byte) (int, map[string][
 // data : interface, 自动将其转成json格式
 //
 // return : statusCode, header, body, error
-func PostJson(url string, headers map[string]string, data interface{}) (int, map[string][]string, []byte, error) {
-
+func PostJsonFull(timeoutSecond int, url string, headers map[string]string, data interface{}) (int, map[string][]string, []byte, error) {
 	if data != nil {
 		if reflect.TypeOf(data).Name() == reflect.TypeOf("").Name() {
-			return DoRequest(POST, url, headers, ApplicationJson, []byte(fmt.Sprintf("%s", data)))
+			return DoRequest(timeoutSecond, POST, url, headers, ApplicationJson, []byte(fmt.Sprintf("%s", data)))
 		}
 		dataJson, _ := json.Marshal(data)
-		return DoRequest(POST, url, headers, ApplicationJson, dataJson)
+		return DoRequest(timeoutSecond, POST, url, headers, ApplicationJson, dataJson)
 	}
-	return DoRequest(POST, url, headers, ApplicationJson, nil)
+	return DoRequest(timeoutSecond, POST, url, headers, ApplicationJson, nil)
 }
 
 // Get: get data from url
 //
 // return : statusCode, header, body, error
-func Get(url string, headers map[string]string) (int, map[string][]string, []byte, error) {
-	return DoRequest(GET, url, headers, "", nil)
+func GetFull(timeoutSecond int, url string, headers map[string]string) (int, map[string][]string, []byte, error) {
+	return DoRequest(timeoutSecond, GET, url, headers, "", nil)
 }
 
 // DoRequest: post data to url
 //
 // return : statusCode, header, body, error
-func DoRequest(method string, url string,
+func DoRequest(timeoutSecond int, method string, url string,
 	headers map[string]string, contentType string,
 	body []byte) (int, map[string][]string, []byte, error) {
 
@@ -67,6 +99,11 @@ func DoRequest(method string, url string,
 	if len(contentType) > 0 {
 		req.Header.Set(ContentType, contentType)
 	}
+
+	if timeoutSecond > 0 { // 设置超时时间
+		client.Timeout = time.Second * time.Duration(timeoutSecond)
+	}
+
 	resp, err := client.Do(req)
 	if ProcessError(err) {
 		return -1, nil, nil, err
