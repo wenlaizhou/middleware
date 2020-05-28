b38d4e8bbfddcd4850977c0644d8c705b39c8967
zhouwl <zhouwl@missfress.cn>
2020-05-28 16:22:00 +0800

    新增metrics工具

--- schedule.go
+++ schedule.go
@@ -8,6 +8,11 @@ import "time"
 func Schedule(name string, timeSchedule int, fun func()) {
 	mLogger.InfoF("注册定时任务: %v 间隔时间: %v秒", name, timeSchedule)
 	go func(timeSchedule int, fun func()) {
+		defer func() {
+			if err := recover(); err != nil {
+				mLogger.ErrorF("schedule 执行错误: %#v", err)
+			}
+		}()
 		for {
 			time.Sleep(time.Second * time.Duration(timeSchedule))
 			fun()
