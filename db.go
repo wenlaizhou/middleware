960a9811416856f369be04f6ce84fd3837aec08b
wenlai <wenlai_zhou@126.com>
2021-09-27 11:41:02 +0800

    新增数据库支持

--- db.go
+++ db.go
@@ -8,7 +8,7 @@ import (
 
 func PooledConnection() *sql.DB {
 	// Opening a driver typically will not attempt to connect to the database.
-	db, err := sql.Open("driver-name", "database=test1")
+	db, err := sql.Open("mysql", "[username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]\n")
 	if err != nil {
 		// This will not be a connection error, but a DSN parse error or
 		// another initialization error.
