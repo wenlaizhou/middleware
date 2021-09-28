55a7252560557fae1383765f0374fa08d46159b6
wenlai <wenlai_zhou@126.com>
2021-09-28 10:36:23 +0800

    新增数据库支持

--- db.go
+++ db.go
@@ -6,16 +6,18 @@ import (
 	"log"
 )
 
-func PooledConnection() *sql.DB {
+// dsn格式: [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+
+func PooledConnection(maxConnections int) *sql.DB {
 	// Opening a driver typically will not attempt to connect to the database.
-	db, err := sql.Open("mysql", "[username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]\n")
+	db, err := sql.Open("mysql", "[username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]")
 	if err != nil {
 		// This will not be a connection error, but a DSN parse error or
 		// another initialization error.
 		log.Fatal(err)
 	}
 	db.SetConnMaxLifetime(0)
-	db.SetMaxIdleConns(50)
-	db.SetMaxOpenConns(50)
+	db.SetMaxIdleConns(maxConnections)
+	db.SetMaxOpenConns(maxConnections)
 	return db
 }
