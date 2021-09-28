09d60076745434d39a8786eb3807f0e397c6462d
wenlai <wenlai_zhou@126.com>
2021-09-28 10:51:01 +0800

    新增数据库支持

--- db.go
+++ db.go
@@ -2,22 +2,20 @@ package middleware
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"log"
 )
 
 // dsn格式: [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
-
-func PooledConnection(maxConnections int) *sql.DB {
+// 使用时需添加驱动: _ "github.com/go-sql-driver/mysql"
+func PooledConnection(driver string, dsn string, maxConnections int) (*sql.DB, error) {
 	// Opening a driver typically will not attempt to connect to the database.
-	db, err := sql.Open("mysql", "[username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]")
+	db, err := sql.Open(driver, dsn)
 	if err != nil {
 		// This will not be a connection error, but a DSN parse error or
 		// another initialization error.
-		log.Fatal(err)
+		return nil, err
 	}
 	db.SetConnMaxLifetime(0)
 	db.SetMaxIdleConns(maxConnections)
 	db.SetMaxOpenConns(maxConnections)
-	return db
+	return db, nil
 }
