d3de187cd5b2514e360e406792bfa54434dc1e33
wenlai <wenlai_zhou@126.com>
2021-11-02 10:55:18 +0800

    新增swagger相关代码

--- swagger.go
+++ swagger.go
@@ -1,6 +1,81 @@
 package middleware
 
+const swaggerHtml = `
+
+<!-- HTML for static distribution bundle build -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Swagger UI</title>
+    <link rel="stylesheet" type="text/css" href="/static/swagger-ui.css" />
+    <style>
+      html
+      {
+        box-sizing: border-box;
+        overflow: -moz-scrollbars-vertical;
+        overflow-y: scroll;
+      }
+
+      *,
+      *:before,
+      *:after
+      {
+        box-sizing: inherit;
+      }
+
+      body
+      {
+        margin:0;
+        background: #fafafa;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div id="swagger-ui"></div>
+
+    <script src="/static/swagger-ui-bundle.js" charset="UTF-8"> </script>
+    <script>
+    window.onload = function() {
+      // Begin Swagger UI call region
+      const ui = SwaggerUIBundle({
+        url: "https://petstore.swagger.io/v2/swagger.json",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        plugins: [
+          SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout"
+      });
+      // End Swagger UI call region
+
+      window.ui = ui;
+    };
+  </script>
+  </body>
+</html>
+
+`
+
 func RegisterSwagger(path string) {
+
+	RegisterHandler("/static/swagger-ui-bundle.js", func(context Context) {
+		context.OK(Js, []byte(SwaggerJs))
+	})
+
+	RegisterHandler("/static/swagger-ui.css", func(context Context) {
+		context.OK(Css, []byte(SwaggerCss))
+	})
+
+	RegisterHandler(path, func(context Context) {
+		context.OK(Html, []byte(swaggerHtml))
+	})
+
 	example := `
 {
     "swagger": "2.0",
