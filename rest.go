11bd3a47cf99b911bdd9c914282694bdbdec7fe3
wenlai <wenlai_zhou@126.com>
2021-11-05 15:02:20 +0800

    新增swagger相关代码

--- rest.go
+++ rest.go
@@ -2,22 +2,14 @@ package middleware
 
 import "encoding/json"
 
-func GenerateSwaggerJson() {
-
-}
-
 // 返回 json数据
 // { code 格式: 0, message : "", data : {}}
 func (this *Context) ApiResponse(code int, message string, data interface{}) error {
-	model := make(map[string]interface{})
-	model["code"] = code
-	model["message"] = message
-	if len(this.restProcessors) > 0 {
-		for _, p := range this.restProcessors {
-			data = p(data)
-		}
+	model := map[string]interface{}{
+		"code":    code,
+		"message": message,
+		"data":    data,
 	}
-	model["data"] = data
 	res, err := json.Marshal(model)
 	if ProcessError(err) {
 		return err
--- swagger_test.go
+++ swagger_test.go
@@ -3,17 +3,4 @@ package middleware
 import "testing"
 
 func TestSwaggerGenerate(t *testing.T) {
-	println(SwaggerGenerate(Swagger{
-		Swagger: "2.0",
-		Info: struct {
-			Title       string `json:"title"`
-			Version     string `json:"version"`
-			Description string `json:"description"`
-		}{
-			Title:       "Middleware",
-			Version:     "2.0.0",
-			Description: "hello world",
-		},
-		Host: "http://localhost",
-	}))
 }
