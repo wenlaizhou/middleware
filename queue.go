e0fda47d7f0c90541bd2ff34c5bf0b1b98170e7c
wenlai <wenlai_zhou@126.com>
2021-11-12 14:20:16 +0800

    新增任务处理框架

--- queue.go
+++ queue.go
@@ -1,5 +1,10 @@
 package middleware
 
+import (
+	"container/list"
+	"sync"
+)
+
 type task struct {
 	Name       string `json:"name"`
 	StartEpoch int64  `json:"startEpoch"`
@@ -7,15 +12,17 @@ type task struct {
 	Runner     func()
 	// new running done error
 	Status string `json:"status"`
+	signal chan bool
 }
 
-func CreateTask(name string, runner func()) task {
+func createTask(name string, runner func()) task {
 	return task{
 		Name:       name,
 		StartEpoch: 0,
 		EndEpoch:   0,
 		Runner:     runner,
 		Status:     "new",
+		signal:     make(chan bool),
 	}
 }
 
@@ -23,18 +30,68 @@ func (thisSelf *task) run() {
 	go func() {
 		defer func() {
 			if r := recover(); r != nil {
+				thisSelf.signal <- false
 				thisSelf.Status = "error"
 			}
 		}()
 		thisSelf.Runner()
 		thisSelf.Status = "done"
+		thisSelf.signal <- true
 	}()
 	thisSelf.Status = "running"
 }
 
 type TaskQueue struct {
-	Queue   []task `json:"queue"`
-	Done    []task `json:"done"`
-	Todo    []task `json:"todo"`
-	Running task   `json:"running"`
+	Queue     *list.List
+	queueLock sync.RWMutex
+	Done      []string
+	Errors    []string
+	Todo      int
+	Running   *task
+	status    string
+}
+
+func CreateTaskQueue() TaskQueue {
+	return TaskQueue{
+		Queue:   list.New(),
+		Done:    []string{},
+		Errors:  []string{},
+		Todo:    0,
+		Running: nil,
+		status:  "new",
+	}
+}
+
+func (thisSelf *TaskQueue) AddTask(name string, runner func()) {
+	thisSelf.queueLock.Lock()
+	defer thisSelf.queueLock.Unlock()
+	thisSelf.Queue.PushBack(createTask(name, runner))
+}
+
+func (thisSelf *TaskQueue) Start() {
+	thisSelf.Done = []string{}
+	thisSelf.Errors = []string{}
+	thisSelf.Todo = thisSelf.Queue.Len()
+	go func() {
+		for e := thisSelf.Queue.Front(); e != nil; e = e.Next() {
+			thisSelf.runner(e.Value.(task))
+		}
+	}()
+}
+
+func (thisSelf *TaskQueue) runner(t task) {
+	t.run()
+	thisSelf.Running = &t
+	thisSelf.Todo -= 1
+	select {
+	case res := <-t.signal:
+		if !res {
+			thisSelf.Errors = append(thisSelf.Errors, t.Name)
+		}
+		thisSelf.Done = append(thisSelf.Done, t.Name)
+	}
+}
+
+func (thisSelf *TaskQueue) Status() {
+
 }
