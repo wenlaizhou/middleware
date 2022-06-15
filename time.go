b70478065972fad6edbc4f36284bd4d1b298d517
wenlai <wenlai_zhou@126.com>
2022-06-15 14:56:57 +0800

    新增信息metrics

--- time.go
+++ time.go
@@ -2,7 +2,9 @@ package middleware
 
 import "time"
 
-// 获取毫秒时间戳
+const TimeFormatForClickhouse = "2006-01-02 15:04:05"
+
+// TimeEpoch 获取毫秒时间戳
 func TimeEpoch() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
 }
