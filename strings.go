178960804e6f63fdc7d664139225c3368e9faf85
wenlai <wenlai_zhou@126.com>
2022-06-22 11:43:07 +0800

    新增格式化字符串接口

--- strings.go
+++ strings.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-// 字符串模板渲染
+// RenderTemplateKV 字符串模板渲染
 func RenderTemplateKV(tpl string, kvs ...interface{}) (string, error) {
 	if len(tpl) <= 0 {
 		return tpl, nil
@@ -31,7 +31,7 @@ func RenderTemplateKV(tpl string, kvs ...interface{}) (string, error) {
 	return buff.String(), nil
 }
 
-// 类似table类型字符串
+// RenderTable 类似table类型字符串
 //
 // 转换为mysql table类型数据
 func RenderTable(data string, width int) []map[string]string {
@@ -64,7 +64,7 @@ func RenderTable(data string, width int) []map[string]string {
 	return res
 }
 
-//判断数据是否为空
+// IsEmpty 判断数据是否为空
 func IsEmpty(param interface{}) bool {
 	switch reflect.TypeOf(param) {
 	case reflect.TypeOf(""):
@@ -72,4 +72,21 @@ func IsEmpty(param interface{}) bool {
 	default:
 		return param == nil
 	}
+}
+
+// StringFormat 格式化字符串
+//
+// ${name}
+func StringFormat(formatter string, params map[string]interface{}) string {
+	res := formatter
+	if len(params) <= 0 {
+		return res
+	}
+	for k, v := range params {
+		if len(k) <= 0 {
+			continue
+		}
+		res = strings.ReplaceAll(res, fmt.Sprintf("${%v}", k), fmt.Sprintf("%v", v))
+	}
+	return res
 }
