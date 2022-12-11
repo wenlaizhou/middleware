b0547d98ef0b9c756e7dc9423605738ae09cef8e
zhouwenlai <zhouwenlai@sensetime.com>
2022-12-11 11:36:13 +0800

    :punch: add bean utils

--- beans.go
+++ beans.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"errors"
 	"reflect"
 	"strings"
 )
@@ -9,45 +10,45 @@ import (
 // 获取bean实例的属性值
 //
 // bean 应为 struct
-func GetProperty(bean interface{}, name string) interface{} {
+func GetProperty(bean interface{}, name string) (interface{}, error) {
 	if bean == nil {
-		return nil
+		return nil, errors.New("bean为空")
 	}
 	name = strings.TrimSpace(name)
 	if len(name) <= 0 {
-		return nil
+		return nil, errors.New("属性名name为空")
 	}
 	tp := reflect.ValueOf(bean)
 	if tp.Kind() != reflect.Struct {
-		return nil
+		return nil, errors.New("bean类型不为struct")
 	}
 	beanVal := reflect.ValueOf(bean)
 	fieldVal := beanVal.FieldByName(name)
-	return fieldVal.Interface()
+	return fieldVal.Interface(), nil
 }
 
 // SetProperty
 // 设置bean实例的属性值
 //
 // bean 应为 指向 struct指针
-func SetProperty(bean interface{}, name string, value interface{}) {
+func SetProperty(bean interface{}, name string, value interface{}) error {
 	if bean == nil {
-		return
+		return errors.New("bean为空")
 	}
 	name = strings.TrimSpace(name)
 	if len(name) <= 0 {
-		return
+		return errors.New("属性name为空")
 	}
 	tp := reflect.ValueOf(bean)
 	if tp.Kind() != reflect.Ptr {
-		return
+		return errors.New("bean应为指向struct指针")
 	}
 	beanVal := reflect.ValueOf(bean).Elem()
 	valVal := reflect.ValueOf(value)
 	fieldVal := beanVal.FieldByName(name)
 	if fieldVal.Kind() != valVal.Kind() {
-		return
+		return errors.New("值类型与bean属性定义类型不符")
 	}
 	fieldVal.Set(reflect.ValueOf(value))
-	return
+	return nil
 }
