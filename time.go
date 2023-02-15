f2dc8f28109fc4bd7539cb7f236e28e2cd704346
zhouwenlai <zhouwenlai@sensetime.com>
2023-02-15 11:53:18 +0800

    :rocket: commit time func

--- time.go
+++ time.go
@@ -1,10 +1,24 @@
 package middleware
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 const TimeFormatForClickhouse = "2006-01-02 15:04:05"
 
 // TimeEpoch 获取毫秒时间戳
 func TimeEpoch() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+func TimeParse() {
+	t, _ := time.ParseInLocation(TimeFormatForClickhouse, "2023-02-15 11:05:00", time.Local)
+	fmt.Println(t)
+	fmt.Println(time.Now())
+	fmt.Println(time.Since(t))
+	bef := time.Now().Add(-30 * time.Second)
+	fmt.Println(time.Since(bef))
+	fmt.Println(time.Since(t))
+	return
 }
